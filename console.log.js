ba9bb5e1b6ae238cd160053c5faa53ea996bfc55
Yaat xd <115164202+YaatxD@users.noreply.github.com>
2023-02-28 13:10:48 -0600

    Ejercicio de Clase

---
+++ console.log.js
@@ -0,0 +1,18 @@
+// ESCRIBIR => console.log en JavaScript
+console.log("Hola Mundo");
+
+// JS ES UN LENGUAJE DE PROGRAMACIÓN DEBILMENTE TIPADO
+// JS PARA HACER USO DE VARIABLES HACEMOS USO DE: let & const
+
+const myName = "Yatziri";
+const myLastName = "Moyao Severiano";
+let myAge = 18;
+let inStudent = true;
+let isTeacher = false;
+
+//PODEMOS CONCATENER VARIABLES Y USARLAS PARA LO QUE DESEEAMOS
+console.log(myName);
+//VOY A CONCATENER MI NOMBRE COMPLETO
+console.log(myName + " " + myLastName);
+// ES6 - Concatenar es ahora más fácil
+console.log(`${myName} ${myLastName}`);
